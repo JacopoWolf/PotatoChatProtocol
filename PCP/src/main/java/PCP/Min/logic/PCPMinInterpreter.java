2ee59751befd42b693d905362df64ce51f794584
JacopoWolf <work.jacopocomparin@gmail.com>
2019-11-09 16:12:22 +0100

    Create PCPMinInterpreter.java

---
+++ PCP/src/main/java/PCP/Min/logic/PCPMinInterpreter.java
@@ -0,0 +1,39 @@
+/*
+ * this is a school project under "The Unlicence".
+ */
+package PCP.Min.logic;
+
+import PCP.*;
+import PCP.data.*;
+import PCP.logic.*;
+import java.util.*;
+
+
+/**
+ *
+ * 
+ */
+public class PCPMinInterpreter implements IPCPInterpreter
+{
+
+    @Override
+    public IPCPdata interpret( byte[] data ) throws PCPException
+    {
+        throw new UnsupportedOperationException();
+    }
+    
+    @Override
+    public void cleanCache()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<IPCPdata> getIncompleteDataList()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    
+    
+}
