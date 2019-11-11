f8177544011d50a9722fe97fda1a4f1a7b7bd99b
JacopoWolf <work.jacopocomparin@gmail.com>
2019-11-11 22:33:32 +0100

    Create IMemoryAccess.java
    to implement later

---
+++ PCP/src/main/java/PCP/logic/IMemoryAccess.java
@@ -0,0 +1,14 @@
+/*
+ * this is a school project under "The Unlicence".
+ */
+package PCP.logic;
+
+
+/**
+ *
+ * @author Jacopo_Wolf
+ */
+public interface IMemoryAccess
+{
+    //todo: implement later
+}
