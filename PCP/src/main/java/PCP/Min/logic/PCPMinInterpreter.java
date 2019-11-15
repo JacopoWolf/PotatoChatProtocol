8fb15887dbb234bfad35a8977afb964c7f1a5eab
Alessio789 <alessio.trentin3@gmail.com>
2019-11-15 12:53:30 +0100

    Implemented interpret for Registration

--- PCP/src/main/java/PCP/Min/logic/PCPMinInterpreter.java
+++ PCP/src/main/java/PCP/Min/logic/PCPMinInterpreter.java
@@ -4,8 +4,11 @@
 package PCP.Min.logic;
 
 import PCP.*;
+import PCP.Min.data.Registration;
+import PCP.PCPException.ErrorCode;
 import PCP.data.*;
 import PCP.logic.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 
@@ -36,11 +39,11 @@ public class PCPMinInterpreter implements IPCPInterpreter
         
         switch( OpCode.getOpCodeFromByte(receivedOpcode) )
         {
+            case Registration:
+                return createRegistrationFromBytes(data);               
             default:
-                return null;
+                throw new PCPException(ErrorCode.PackageMalformed);
         }
-        
-        
     }
 
     @Override
@@ -54,7 +57,46 @@ public class PCPMinInterpreter implements IPCPInterpreter
     {
         this.incompleteDataList = incompleteDataList;
     }
-
     
+    private void addIncompleteData ( IPCPData incompleteData ) 
+    {
+        synchronized ( incompleteDataList )
+        {
+            this.incompleteDataList.add(incompleteData);
+        }
+    }
     
+    private Registration createRegistrationFromBytes( byte[] data ) throws PCPException 
+    {
+        
+        Registration registration = new Registration(null, null);
+        
+        int i = 0;
+        int start = 2;
+        
+        ArrayList<byte[]> list = new ArrayList<>();
+        
+        for ( byte b : data ) 
+        {
+            if ( b == 0 ) 
+            {
+                list.add(Arrays.copyOfRange(data, start, i));
+                start = ++i;
+            }
+            if ( b == 040 )
+                throw new PCPException(ErrorCode.InvalidAlias);
+            i++;
+        }
+        
+        if ( list.get(0).length < 6 || list.get(0).length > 32 )
+            throw new PCPException(ErrorCode.InvalidAlias);
+        
+        if ( list.get(1)[0] != 0 && (list.get(1).length < 6 || list.get(1).length > 32))
+            throw new PCPException(ErrorCode.InvalidRoomName);
+        
+        registration.setAlias(new String(list.get(0)));
+        registration.setTopic(new String(list.get(1)));
+       
+        return registration;
+    }
 }
